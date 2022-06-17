c2b7084df0aab7dd966dfa11a4ffe683c68b7eca
AleksandrArkM <melua.aleksandr@gmail.com>
2022-06-17 19:32:05 +0300

    optimized

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,8 +3,8 @@ fun main() {
     println("был(а) в сети ${agoToText(3600)}")
     println("------")
     println("Вторая задача")
-    println(comissionPay("Mastercard", 0, 400))
-    println(comissionPay("Мир", 0, 400))
+    println(comissionPay("Mastercard"))
+    println(comissionPay("Мир"))
 }
 
 fun agoToText(timeSeconds: Int): String {
@@ -13,7 +13,29 @@ fun agoToText(timeSeconds: Int): String {
     val timeMinutes = timeSeconds/60
     val timeHours = timeMinutes/60
 
-    minutesText = when {
+    return when (timeSeconds){
+        in 0..60 -> "только что"
+        in 61..60*60 -> "$timeMinutes ${minutesText(timeMinutes)} назад"
+        in (60*60)+1..24*60*60 -> "$timeHours ${hoursText(timeHours)} назад"
+        in (24*60*60)+1..24*60*60*2 -> "сегодня"
+        in (24*60*60*2)+1..(24*60*60*3) -> "вчера"
+        else -> "давно"
+    }
+}
+
+fun comissionPay(cardType: String): Int {
+    val summTrans = 400
+    val summPrevTrans = 0
+
+    return when (cardType) {
+        "Mastercard", "Maestro" -> if ((summTrans > 300) && ((summTrans+summPrevTrans) < 75000)) 0 else (summTrans*100/1000*6)+20*100
+        "Visa", "Мир" -> if((summTrans/10000*75) > 35) summTrans*100/10000*75 else 35
+        "VK Pay" -> 0
+        else -> 0
+    }
+}
+fun minutesText (timeMinutes: Int): String {
+    return when {
         (timeMinutes > 10) && (timeMinutes < 20) -> "минут"
         timeMinutes%10 == 1 -> "минуту"
         timeMinutes%10 == 2 -> "минуты"
@@ -22,8 +44,9 @@ fun agoToText(timeSeconds: Int): String {
         timeMinutes%10 == 5 -> "минут"
         else -> "минут"
     }
-
-    hoursText = when {
+}
+fun hoursText(timeHours: Int) : String {
+    return when {
         timeHours == 1 -> "час"
         timeHours == 2 -> "часа"
         timeHours == 3 -> "часа"
@@ -32,24 +55,5 @@ fun agoToText(timeSeconds: Int): String {
         timeHours == 22 -> "часа"
         timeHours == 23 -> "часа"
         else -> "часов"
-    }
-
-    return when (timeSeconds){
-        in 0..60 -> "только что"
-        in 61..60*60 -> "$timeMinutes $minutesText назад"
-        in (60*60)+1..24*60*60 -> "$timeHours $hoursText назад"
-        in (24*60*60)+1..24*60*60*2 -> "сегодня"
-        in (24*60*60*2)+1..(24*60*60*3) -> "вчера"
-        else -> "давно"
-    }
-}
-
-fun comissionPay(cardType: String, summPrevTrans: Int, summTrans: Int): Int {
-
-    return when (cardType) {
-        "Mastercard", "Maestro" -> if ((summTrans > 300) && ((summTrans+summPrevTrans) < 75000)) 0 else (summTrans*100/1000*6)+20*100
-        "Visa", "Мир" -> if((summTrans/10000*75) > 35) summTrans*100/10000*75 else 35
-        "VK Pay" -> 0
-        else -> 0
     }
 }
